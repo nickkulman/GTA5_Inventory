73a24f6c6f8529bd8e85c04ecef68b150990340c
Nick Kulman <nickkulman@gmail.com>
2021-03-01 14:31:55 +0300

    Add files via upload

---
+++ Inventory/main.js
@@ -0,0 +1,10 @@
+
+$('#1').on('click', function(){
+        $('#1_1').addClass('hidden');
+        $('#1_2').removeClass('hidden');
+        });
+  
+$('#2').on('click', function(){
+        $('#1_2').addClass('hidden');
+        $('#1_1').removeClass('hidden');
+        });
